7bf7fd4df3af5f8adf6cbb67a4d5573b83b8c801
miquel regidor <miquel@comertis.com>
2022-08-11 17:28:06 +0200

    projectBelongs01

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,19 +3,32 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/main", name="app_home")
+     * @Route("/main", name="main")
      */
-    public function index(): JsonResponse
+    public function index(): Response
     {
-        return $this->json([
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+        /*return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/HomeController.php',
-        ]);
+        ]);*/
+    }
+    /**
+     * @Route("/start{name?}", name="start")
+     */
+    public function start(): Response
+    {
+        return new Response('<h1>Ok</h1>');
+        /*return $this->render('main/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);*/
     }
 }
